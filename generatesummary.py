0b77d449d41cb6a0f7fe85abb3538d9ca33013ec
Gergely Imreh <imrehg@gmail.com>
2015-04-04 22:42:38 +0800

    generatesummary: calculate historical data

--- generatesummary.py
+++ generatesummary.py
@@ -28,6 +28,75 @@ def loadServerList(inputFile='servers.csv', httpsOnly=True):
                     sites += [{'url': host.netloc, 'name': name, 'link': link}]
     return sites
 
+def parsedate(site, date):
+    """ Calculat scores for a site and a specific date
+
+    Keyword arguments:
+    site -- site data dict
+    date -- datetime.date in question
+    """
+    indir = str(date)
+    serverName = site['url']
+    filename = os.path.join(indir, serverName + ".json.gz")
+    thisResult = { 'name': site['name'] }
+
+    if not os.path.exists(filename):
+        thisResult['lowGrade'] = '?'
+        thisResult['link'] = site['link']
+        thisResult['url'] = serverName
+        thisResult['endpoints'] = []
+        return thisResult
+
+    f = gzip.open(filename, 'rb')
+    scanresult = json.loads(f.read())
+    f.close()
+    endpoints = scanresult[0]['endpoints']
+
+    thisResult['link'] = site['link']
+    thisResult['url'] = serverName
+    ends = []
+    for e in endpoints:
+        try:
+            grade = e['grade']
+        except KeyError:
+            grade = 'X'
+
+        try:
+            warnings = e['hasWarnings']
+        except KeyError:
+            warnings = None
+
+        try:
+            rc4 = e['details']['supportsRc4']
+        except KeyError:
+            rc4 = None
+
+        try:
+            poodle = e['details']['poodleTls'] > 1
+        except KeyError:
+            poodle = None
+
+        try:
+            beast = e['details']['vulnBeast']
+        except KeyError:
+            beast = None
+
+        ends += [{'grade': grade,
+                  'ipAddress': e['ipAddress'],
+                  'hasWarnings': warnings,
+                  'statusMessage': e['statusMessage'],
+                  'rc4': rc4,
+                  'poodle': poodle,
+                  'beast': beast,
+              }]
+        if ('grade' not in thisResult) or (grade < thisResult['lowGrade']):
+            if grade != 'X':
+                thisResult['lowGrade'] = grade
+    if 'lowGrade' not in thisResult:
+        thisResult['lowGrade'] = 'X'
+    thisResult['endpoints'] = ends
+    return thisResult
+
 if __name__ == '__main__':
     import argparse
     import ConfigParser
@@ -39,76 +108,27 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     # Configuration
-    config = ConfigParser.SafeConfigParser({'timezone': 'utc'})
+    config = ConfigParser.SafeConfigParser({'timezone': 'utc', 'wayback': 60})
     config.read(args.config)
 
     today = datetime.now(pytz.timezone(config.get('Common', 'timezone'))).date()
     indir = str(today)
     output = {"update": str(today)};
     results = []
+    grades = {}
     sites = loadServerList(httpsOnly=False)
-    for s in sites:
-        serverName = s['url']
-        filename = os.path.join(indir, serverName + ".json.gz")
-
-        thisResult = { 'name': s['name'] }
-
-        if not os.path.exists(filename):
-            thisResult['lowGrade'] = 'X'
-            thisResult['link'] = s['link']
-            thisResult['url'] = serverName
-            thisResult['endpoints'] = []
-            results += [thisResult]
-            continue
-
-        f = gzip.open(filename, 'rb')
-        scanresult = json.loads(f.read())
-        f.close()
-        endpoints = scanresult[0]['endpoints']
-
-        thisResult['link'] = s['link']
-        thisResult['url'] = serverName
-        ends = []
-        for e in endpoints:
-            try:
-                grade = e['grade']
-            except KeyError:
-                grade = 'X'
-
-            try:
-                warnings = e['hasWarnings']
-            except KeyError:
-                warnings = None
-
-            try:
-                rc4 = e['details']['supportsRc4']
-            except KeyError:
-                rc4 = None
-
-            try:
-                poodle = e['details']['poodleTls'] > 1
-            except KeyError:
-                poodle = None
-
-            try:
-                beast = e['details']['vulnBeast']
-            except KeyError:
-                beast = None
-
-            ends += [{'grade': grade,
-                      'ipAddress': e['ipAddress'],
-                      'hasWarnings': warnings,
-                      'statusMessage': e['statusMessage'],
-                      'rc4': rc4,
-                      'poodle': poodle,
-                      'beast': beast,
-                  }]
-            if ('grade' not in thisResult) or (grade < thisResult['lowGrade']):
-                if grade != 'X':
-                    thisResult['lowGrade'] = grade
-        if 'lowGrade' not in thisResult:
-            thisResult['lowGrade'] = 'X'
-        thisResult['endpoints'] = ends
+    for idx, s in enumerate(sites):
+        thisResult = parsedate(s, today)
         results += [thisResult]
+        grades[idx] = [thisResult['lowGrade']]
+    wayback = config.getint('Analyze', 'wayback');
+    for w in range(1, wayback):
+        oldday = today - timedelta(days = w)
+        for idx, s in enumerate(sites):
+            oldResult = parsedate(s, oldday)
+            grades[idx].insert(0, oldResult['lowGrade'])
+
+    for idx, s in enumerate(sites):
+        results[idx]['wayback'] = grades[idx]
     output['results'] = results
     print json.dumps(output)
